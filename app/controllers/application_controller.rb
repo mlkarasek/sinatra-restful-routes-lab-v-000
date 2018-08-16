5e2a335263cfd0c7d89127224ce2324d65e19120
Monica  Karasek <mlkarasek@gmail.com>
2018-08-16 01:21:55 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,20 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
   get '/' do
     erb :index
   end
 
   get '/recipes/new' do
     erb :new
-    @recipe.save
   end
+
+  post '/recipes' do 
+    @recipe = Recipe.new(name: params[:name], ingredients: params[:ingredients])
+    @recipe.save
+    redirect "recipes/#{{@recipe.id}}"
+  end 
 
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
