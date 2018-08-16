f278aca95a496f267daa93d9efbf1c79ffccaa93
Monica  Karasek <mlkarasek@gmail.com>
2018-08-16 01:16:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,8 +37,8 @@ end
 
 delete '/recipes/:id/delete' do
   @recipe = Recipe.find_by_id(params[:id])
-  @recipe.delete 
+  @recipe.delete
   redirect to '/recipes'
-    end 
+    end
   end 
-end 
+end
